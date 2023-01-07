b401ddc75c13ac88070f6ee640b64a44d6e90636
Parthipan <parthiban@sparkouttech.com>
2023-01-07 09:28:59 +0530

    subscription added

---
+++ in-App Purschase/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  in-App Purschase
+//
+//  Created by Sparkout on 04/01/23.
+//
+
+import StoreKit
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, SKProductsRequestDelegate, SKPaymentTransactionObserver {
+    
+    var models = [SKProduct]()
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+        SKPaymentQueue.default().add(self)
+        fetchProducts()
+    }
+    // TableView Delegates
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let product = models[indexPath.row]
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.numberOfLines = 0
+       
+        cell.textLabel?.text = "Monthly Subscription - \(product.priceLocale.currencySymbol ?? "$")\(product.price)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let payment = SKPayment(product: models[indexPath.row])
+        SKPaymentQueue.default().add(payment)
+    }
+    // Products
+    enum Products: String, CaseIterable {
+        case MonthlyTen = "MonthlySubscriptionTen"
+        case Monthly15 = "monthly15"
+        case monthlySubscription = "MonthlySubscription"
+    }
+    
+    func fetchProducts() {
+        let request = SKProductsRequest(productIdentifiers: Set(Products.allCases.compactMap({ $0.rawValue })))
+        request.delegate = self
+        request.start()
+    }
+    // SkPaymentRequst Delegate
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        DispatchQueue.main.async {
+            print("Count:\(response.products)")
+            self.models = response.products
+            self.tableView.reloadData()
+        }
+    }
+    // SkpaymentTrasactionObserver delegate
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        transactions.forEach {
+            switch $0.transactionState {
+            case .purchasing:
+                print("Purchasing")
+            case .purchased:
+                print("purchased")
+                SKPaymentQueue.default().finishTransaction($0)
+            case .failed:
+                print("not yet purchased")
+            case .restored:
+                break
+            case .deferred:
+                break
+            @unknown default:
+                break
+            }
+        }
+    }
+  
+}
---
+++ in-App PurschaseUITests/in_App_PurschaseUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  in_App_PurschaseUITestsLaunchTests.swift
+//  in-App PurschaseUITests
+//
+//  Created by Sparkout on 04/01/23.
+//
+
+import XCTest
+
+class in_App_PurschaseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
